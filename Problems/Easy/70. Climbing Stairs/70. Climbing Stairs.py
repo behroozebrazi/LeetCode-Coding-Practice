7bd832be0d31c8c79164ecc9340bb5b76267971e
behroozebrazi <behber@berkeley.edu>
2026-08-03 14:09:35 -0700

    update 70. Climbing Stairs

--- Problems/Easy/70. Climbing Stairs/70. Climbing Stairs.py
+++ Problems/Easy/70. Climbing Stairs/70. Climbing Stairs.py
@@ -3,19 +3,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        memo = dict()
+        if n <= 2:
+            return n
 
-        def step(remainSteps):
-            if remainSteps == 0:
-                return 1
-            elif remainSteps < 0:
-                return 0
+        first = 1
+        second = 2
 
-            if remainSteps not in memo:
-                memo[remainSteps] = step(remainSteps - 1) + step(remainSteps - 2)
+        for _ in range(3, n + 1):
+            first, second = second, first + second
 
-            return memo[remainSteps]
-
-        step(n)
-
-        return memo[n]
+        return second
