1787c7d6fccfe8fd6a4004691fdd456899cddc58
behroozebrazi <behber@berkeley.edu>
2026-08-03 09:17:43 -0700

    70. Climbing Stairs

--- Problems/Easy/70. Climbing Stairs/70. Climbing Stairs.py
+++ Problems/Easy/70. Climbing Stairs/70. Climbing Stairs.py
@@ -1,2 +1,21 @@
 # https://leetcode.com/problems/climbing-stairs/
 
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo = dict()
+
+        def step(remainSteps):
+            if remainSteps == 0:
+                return 1
+            elif remainSteps < 0:
+                return 0
+
+            if remainSteps not in memo:
+                memo[remainSteps] = step(remainSteps - 1) + step(remainSteps - 2)
+
+            return memo[remainSteps]
+
+        step(n)
+
+        return memo[n]
