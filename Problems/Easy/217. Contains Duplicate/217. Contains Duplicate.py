e138b3415d99713c09c4f701eeb46bb7bd2bbd07
behroozebrazi <behber@berkeley.edu>
2026-08-04 06:08:47 -0700

    217. Contains Duplicate

--- Problems/Easy/217. Contains Duplicate/217. Contains Duplicate.py
+++ Problems/Easy/217. Contains Duplicate/217. Contains Duplicate.py
@@ -1,2 +1,13 @@
 # https://leetcode.com/problems/contains-duplicate/
 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        singleNums = set()
+
+        for num in nums:
+            if num in singleNums:
+                return True
+
+            singleNums.add(num)
+
+        return False
