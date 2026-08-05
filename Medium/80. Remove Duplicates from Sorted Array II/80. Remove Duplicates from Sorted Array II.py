6f1632728b3e04d1b1c8383fff4480e360a99314
behroozebrazi <behber@berkeley.edu>
2026-08-05 10:38:03 -0700

    80. Remove Duplicates from Sorted Array II

--- Medium/80. Remove Duplicates from Sorted Array II/80. Remove Duplicates from Sorted Array II.py
+++ Medium/80. Remove Duplicates from Sorted Array II/80. Remove Duplicates from Sorted Array II.py
@@ -1,2 +1,41 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        if len(nums) < 3:
+            return len(nums)
+
+        left = 1
+        right = 2
+
+        while right < len(nums):
+
+            if nums[left - 1] != nums[right] or nums[left] != nums[right]:
+                left += 1
+                nums[left] = nums[right]
+
+            right += 1
+
+        return left + 1
+
+
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+
+#         if len(nums) < 3:
+#             return len(nums)
+
+#         left = 1
+#         right = 2
+
+#         while right < len(nums):
+
+#             if nums[left - 1] == nums[right] and nums[left] == nums[right]:
+#                 right += 1
+#             else:
+#                 left += 1
+#                 nums[left] = nums[right]
+#                 right += 1
+
+#         return left + 1
