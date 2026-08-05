1f9ffb5b10c85352471de1cad7ea5899bfd4c241
behroozebrazi <behber@berkeley.edu>
2026-08-05 15:24:33 -0700

    274. H-Index

--- Medium/274. H-Index/274. H-Index.py
+++ Medium/274. H-Index/274. H-Index.py
@@ -1,2 +1,15 @@
 # https://leetcode.com/problems/h-index
 
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+        if len(citations) == 1 and citations[0] > 0:
+            return 1
+
+        citations.sort(reverse=True)
+
+        for i, citation in enumerate(citations):
+            if i >= citation:
+                return i
+
+        return len(citations)
