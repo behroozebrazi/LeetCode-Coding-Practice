0ea9fe950bb83dbb850c1af11576f3a57ca6dfc9
behroozebrazi <behber@berkeley.edu>
2026-08-04 20:29:18 -0700

    27. Remove Element

--- Easy/27. Remove Element/27. Remove Element.py
+++ Easy/27. Remove Element/27. Remove Element.py
@@ -1,2 +1,18 @@
 # https://leetcode.com/problems/remove-element
 
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        
+        left = right = 0
+
+        while right < len(nums):
+
+            while right < len(nums) and nums[right] == val:
+                right += 1
+
+            if right < len(nums):
+                nums[left] = nums[right]
+                left += 1
+                right += 1
+
+        return left
