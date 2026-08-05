100432c37e9d67a3fae13b0f1a0fcf22eca94c0f
behroozebrazi <behber@berkeley.edu>
2026-08-05 10:49:16 -0700

    169. Majority Element

--- Easy/169. Majority Element/169. Majority Element.py
+++ Easy/169. Majority Element/169. Majority Element.py
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/majority-element
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        halfLength = floor(len(nums) / 2)
+        counter = Counter(nums)
+
+        for key, value in counter.items():
+            if halfLength < value:
+                return key
