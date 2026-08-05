d483e08f0817b84d1a425ce31894ffdf0578040c
behroozebrazi <behber@berkeley.edu>
2026-08-05 13:03:45 -0700

    55. Jump Game

--- Medium/55. Jump Game/55. Jump Game.py
+++ Medium/55. Jump Game/55. Jump Game.py
@@ -1,2 +1,18 @@
 # https://leetcode.com/problems/jump-game
 
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+
+        maxReach = 0
+        lastIndex = len(nums) - 1
+
+        for i, num in enumerate(nums):
+            if maxReach < i:
+                return False
+
+            maxReach = max(maxReach, num + i)
+
+            if lastIndex <= maxReach:
+                return True
+
+        return True
