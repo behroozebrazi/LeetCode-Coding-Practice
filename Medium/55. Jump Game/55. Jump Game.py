20253a77acd9008b0cede0913e8706ca7e903976
behroozebrazi <behber@berkeley.edu>
2026-08-05 13:10:09 -0700

    55. Jump Game

--- Medium/55. Jump Game/55. Jump Game.py
+++ Medium/55. Jump Game/55. Jump Game.py
@@ -7,12 +7,29 @@ class Solution:
         lastIndex = len(nums) - 1
 
         for i, num in enumerate(nums):
-            if maxReach < i:
-                return False
+            if i <= maxReach:
+                maxReach = max(maxReach, num + i)
+                if lastIndex <= maxReach:
+                    return True
+            else:
+                break
 
-            maxReach = max(maxReach, num + i)
+        return False
 
-            if lastIndex <= maxReach:
-                return True
 
-        return True
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+
+#         maxReach = 0
+#         lastIndex = len(nums) - 1
+
+#         for i, num in enumerate(nums):
+#             if maxReach < i:
+#                 return False
+
+#             maxReach = max(maxReach, num + i)
+
+#             if lastIndex <= maxReach:
+#                 return True
+
+#         return True
