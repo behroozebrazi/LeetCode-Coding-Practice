eefa4b95e5435d51ac7b53cf29a3e3675eacdc1f
behroozebrazi <behber@berkeley.edu>
2026-08-05 14:58:19 -0700

    45. Jump Game II

--- Easy/26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.py
+++ Easy/26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.py
@@ -7,14 +7,31 @@ class Solution:
             return len(nums)
 
         left = 0
-        right = 1
 
-        while right < len(nums):
+        for right in range(1, len(nums)):
 
             if nums[left] != nums[right]:
                 left += 1
                 nums[left] = nums[right]
 
-            right += 1
-
         return left + 1
+
+
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+
+#         if len(nums) < 2:
+#             return len(nums)
+
+#         left = 0
+#         right = 1
+
+#         while right < len(nums):
+
+#             if nums[left] != nums[right]:
+#                 left += 1
+#                 nums[left] = nums[right]
+
+#             right += 1
+
+#         return left + 1
--- Medium/45. Jump Game II/45. Jump Game II.py
+++ Medium/45. Jump Game II/45. Jump Game II.py
@@ -1,2 +1,25 @@
 # https://leetcode.com/problems/jump-game-ii
 
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        jumps = 1
+        maxReach = 0
+        nextMaxReach = nums[0]
+        lastIndex = len(nums) - 1
+
+        for i in range(1, len(nums)):
+
+            if nextMaxReach < i:
+                return 0
+
+            if lastIndex <= nextMaxReach:
+                return jumps
+
+            if maxReach < i:
+                maxReach = nextMaxReach
+                jumps += 1
+
+            nextMaxReach = max(nextMaxReach, nums[i] + i)
+
+        return 0
