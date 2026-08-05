2a554d1802e3ae7a3ced7db21c7e8fa8b701dba6
behroozebrazi <behber@berkeley.edu>
2026-08-05 12:13:33 -0700

    122. Best Time to Buy and Sell Stock II

--- Medium/122. Best Time to Buy and Sell Stock II/122. Best Time to Buy and Sell Stock II.py
+++ Medium/122. Best Time to Buy and Sell Stock II/122. Best Time to Buy and Sell Stock II.py
@@ -1,2 +1,19 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        totalProfit = 0
+        profit = 0
+        buy = prices[0]
+
+        for i in range(1, len(prices)):
+
+            if prices[i - 1] > prices[i]:
+                buy = prices[i]
+                totalProfit += profit
+                profit = 0
+            else:
+                profit = max(profit, prices[i] - buy)
+
+        return totalProfit + profit
