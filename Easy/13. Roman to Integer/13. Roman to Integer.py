757891103c7118342c9a219662ba3732c59e59c6
behroozebrazi <behber@berkeley.edu>
2026-08-06 14:08:15 -0700

    13. Roman to Integer

--- Easy/13. Roman to Integer/13. Roman to Integer.py
+++ Easy/13. Roman to Integer/13. Roman to Integer.py
@@ -1,2 +1,26 @@
 # https://leetcode.com/problems/roman-to-integer
 
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+        prev = 0
+
+        for letter in s[::-1]:
+            curr = roman[letter]
+
+            result += curr if curr >= prev else -curr
+
+            prev = curr
+
+        return result
