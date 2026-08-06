4b0efe9050f26c2d98d2ebd50f44cb576c028ca6
behroozebrazi <behber@berkeley.edu>
2026-08-06 10:27:12 -0700

    134. Gas Station

--- Medium/134. Gas Station/134. Gas Station.py
+++ Medium/134. Gas Station/134. Gas Station.py
@@ -1,2 +1,19 @@
 # https://leetcode.com/problems/gas-station
 
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        totalGas = 0
+        remainGas = 0
+        startIndex = 0
+
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            totalGas += diff
+            remainGas += diff
+
+            if remainGas < 0:
+                remainGas = 0
+                startIndex = i + 1
+
+        return startIndex if totalGas >= 0 else -1
