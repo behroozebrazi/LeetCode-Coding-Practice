04c90f4b3f602ac060d25c438378255357d52418
behroozebrazi <behber@berkeley.edu>
2026-08-06 10:39:04 -0700

    update 169. Majority Element with additional algorithm

--- Easy/169. Majority Element/169. Majority Element.py
+++ Easy/169. Majority Element/169. Majority Element.py
@@ -9,3 +9,18 @@ class Solution:
         for key, value in counter.items():
             if halfLength < value:
                 return key
+
+
+        # majority_candidate = None
+        # count = 0
+
+        # for num in nums:
+        #     if count == 0:
+        #         majority_candidate = num
+            
+        #     if num == majority_candidate:
+        #         count += 1
+        #     else:
+        #         count -= 1
+
+        # return majority_candidate
