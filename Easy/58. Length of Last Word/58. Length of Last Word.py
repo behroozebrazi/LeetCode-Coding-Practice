b1e05cf73b5a309358ea4ac3f5bb035676ef5d44
behroozebrazi <behber@berkeley.edu>
2026-08-06 15:36:14 -0700

    58. Length of Last Word

--- Easy/58. Length of Last Word/58. Length of Last Word.py
+++ Easy/58. Length of Last Word/58. Length of Last Word.py
@@ -1,2 +1,16 @@
 # https://leetcode.com/problems/length-of-last-word
 
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+
+        for letter in s[::-1]:
+
+            if letter != ' ':
+                length += 1
+
+            else:
+                if length > 0:
+                    return length
+
+        return length
