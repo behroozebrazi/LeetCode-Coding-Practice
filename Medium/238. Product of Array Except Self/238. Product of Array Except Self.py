8155bcf96db688094ebffdce76fec096e1365888
behroozebrazi <behber@berkeley.edu>
2026-08-06 09:32:56 -0700

    238. Product of Array Except Self

--- Medium/238. Product of Array Except Self/238. Product of Array Except Self.py
+++ Medium/238. Product of Array Except Self/238. Product of Array Except Self.py
@@ -1,2 +1,18 @@
 # https://leetcode.com/problems/product-of-array-except-self
 
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        result = []
+
+        prefix = 1
+        for num in nums:
+            result.append(prefix)
+            prefix *= num
+
+        suffix = 1
+        for i in range(len(nums) -1 , -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
+
+        return result
