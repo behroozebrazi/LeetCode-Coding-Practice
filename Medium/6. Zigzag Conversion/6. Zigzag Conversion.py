a70a3b6c5e7e69f496e74d5bfa035f7ce3f3372d
behroozebrazi <behber@berkeley.edu>
2026-08-06 19:51:45 -0700

    6. Zigzag Conversion

--- Medium/6. Zigzag Conversion/6. Zigzag Conversion.py
+++ Medium/6. Zigzag Conversion/6. Zigzag Conversion.py
@@ -1,2 +1,24 @@
 # https://leetcode.com/problems/zigzag-conversion
 
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+
+        if numRows <= 1 or not s:
+            return s
+
+        rowIndex = 0
+        rowDirection = 0
+        lastRowIndex = numRows - 1
+        rows = [""] * numRows
+
+        for ch in s:
+            rowIndex += rowDirection
+
+            rows[rowIndex] += ch
+
+            if rowIndex == 0:
+                rowDirection = 1
+            elif rowIndex == lastRowIndex:
+                rowDirection = -1
+
+        return "".join(rows)
