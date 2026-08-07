26f19567f46d1a5a58f61aa5631cbef60a3859e0
behroozebrazi <behber@berkeley.edu>
2026-08-07 14:00:41 -0700

    392. Is Subsequence

--- Easy/392. Is Subsequence/392. Is Subsequence.py
+++ Easy/392. Is Subsequence/392. Is Subsequence.py
@@ -1,2 +1,18 @@
 # https://leetcode.com/problems/is-subsequence
 
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        pointerS = 0
+        lengthS = len(s)
+
+        if lengthS == 0:
+            return True
+
+        for ch in t:
+            if ch == s[pointerS]:
+                pointerS += 1
+                if pointerS >= lengthS:
+                    return True
+
+        return False
