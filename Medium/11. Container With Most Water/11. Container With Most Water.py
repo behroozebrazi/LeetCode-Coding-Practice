a1a64e75c9471c2e59c8a683448d07f20c664f12
behroozebrazi <behber@berkeley.edu>
2026-08-07 14:43:54 -0700

    11. Container With Most Water

--- Medium/11. Container With Most Water/11. Container With Most Water.py
+++ Medium/11. Container With Most Water/11. Container With Most Water.py
@@ -1,2 +1,25 @@
 # https://leetcode.com/problems/container-with-most-water
 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+
+        left = 0
+        right = len(height) - 1
+        localMaxWater = 0
+        maxWater = 0
+
+        while left < right:
+            diff = height[left] - height[right]
+
+            if diff > 0:
+                localMaxWater = (right - left) *  height[right]
+                right -= 1
+
+            else:
+                localMaxWater = (right - left) *  height[left]
+                left += 1
+
+            if maxWater < localMaxWater:
+                maxWater = localMaxWater
+
+        return maxWater
