47ac08a02c25ca6fa908d770b6a3bfa3c259f00c
behroozebrazi <behber@berkeley.edu>
2026-08-08 11:51:50 -0700

    209. Minimum Size Subarray Sum

--- Medium/209. Minimum Size Subarray Sum/209. Minimum Size Subarray Sum.py
+++ Medium/209. Minimum Size Subarray Sum/209. Minimum Size Subarray Sum.py
@@ -1,2 +1,18 @@
 # https://leetcode.com/problems/minimum-size-subarray-sum
 
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+
+        minLength = len(nums) + 1
+        sumNums = 0
+        left = 0
+
+        for right, num in enumerate(nums):
+            sumNums += num
+
+            while sumNums >= target:
+                minLength = min(minLength, right - left + 1)
+                sumNums -= nums[left]
+                left += 1
+
+        return minLength if minLength <= len(nums) else 0
