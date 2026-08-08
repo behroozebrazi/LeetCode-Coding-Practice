2acaa64f6ed82daeee7d933a14aeadb8e71144e8
behroozebrazi <behber@berkeley.edu>
2026-08-07 17:43:41 -0700

    15. 3Sum

--- Medium/15. 3Sum/15. 3Sum.py
+++ Medium/15. 3Sum/15. 3Sum.py
@@ -1,2 +1,43 @@
 # https://leetcode.com/problems/3sum
 
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+
+        nums.sort()
+        result = []
+
+        for i in range(len(nums) - 2):
+
+            # Skip duplicate first numbers
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # Since array is sorted, no possible triplet
+            if nums[i] > 0:
+                break
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    # Skip duplicates
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
