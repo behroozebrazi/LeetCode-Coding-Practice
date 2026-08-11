53e6579c950940bf445a040b02349a74807bddeb
behroozebrazi <behber@berkeley.edu>
2026-08-10 23:24:28 -0700

    36. Valid Sudoku

--- Medium/36. Valid Sudoku/36. Valid Sudoku.py
+++ Medium/36. Valid Sudoku/36. Valid Sudoku.py
@@ -1,2 +1,26 @@
 # https://leetcode.com/problems/valid-sudoku
 
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        seen = set()
+
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+
+                if num == ".":
+                    continue
+
+                row = ("row", r, num)
+                col = ("col", c, num)
+                box = ("box", r // 3, c // 3, num)
+
+                if row in seen or col in seen or box in seen:
+                    return False
+
+                seen.add(row)
+                seen.add(col)
+                seen.add(box)
+
+        return True
