571ba2cd615d20225f81c1f429934d6e9374e375
behroozebrazi <behber@berkeley.edu>
2026-08-12 15:51:58 -0700

    290. Word Pattern

--- Easy/290. Word Pattern/290. Word Pattern.py
+++ Easy/290. Word Pattern/290. Word Pattern.py
@@ -1,2 +1,25 @@
 # https://leetcode.com/problems/word-pattern
 
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+
+        dict1 = dict()
+        set1 = set()
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        for i, word in enumerate(words):
+            letter = pattern[i]
+
+            if letter in dict1:
+                if dict1[letter] != word:
+                    return False
+            else:
+                if word in set1:
+                    return False
+                dict1[letter] = word
+                set1.add(word)
+
+        return True
