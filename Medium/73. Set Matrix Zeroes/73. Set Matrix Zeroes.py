35455f85e91b1775ac381aff14bddcb7529fc648
behroozebrazi <behber@berkeley.edu>
2026-08-12 15:18:31 -0700

    73. Set Matrix Zeroes

--- Medium/73. Set Matrix Zeroes/73. Set Matrix Zeroes.py
+++ Medium/73. Set Matrix Zeroes/73. Set Matrix Zeroes.py
@@ -1,2 +1,26 @@
 # https://leetcode.com/problems/set-matrix-zeroes
 
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        zeroRows = set()
+        zeroCols = set()
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 0:
+                    zeroRows.add(row)
+                    zeroCols.add(col)
+
+        for row in zeroRows:
+            for col in range(n):
+                 matrix[row][col] = 0
+
+        for col in zeroCols:
+            for row in range(m):
+                matrix[row][col] = 0
