a991cc3809ed9cec3b01d731597016ff01e52d2d
behroozebrazi <behber@berkeley.edu>
2026-08-12 15:54:54 -0700

    242. Valid Anagram

--- Easy/242. Valid Anagram/242. Valid Anagram.py
+++ Easy/242. Valid Anagram/242. Valid Anagram.py
@@ -1,2 +1,9 @@
 # https://leetcode.com/problems/valid-anagram
 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        return Counter(s) == Counter(t)
