6ebd4900ae8d14c953b5bfa8f0a77081f5d7358a
behroozebrazi <behber@berkeley.edu>
2026-08-12 16:57:52 -0700

    202. Happy Number

--- Easy/202. Happy Number/202. Happy Number.py
+++ Easy/202. Happy Number/202. Happy Number.py
@@ -1,2 +1,20 @@
 # https://leetcode.com/problems/happy-number
 
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        seen = set()
+
+        while n > 1:
+            seen.add(n)
+
+            sumNum = 0
+            while n > 0:
+                sumNum += (n % 10) ** 2
+                n = n // 10
+            n = sumNum
+
+            if n in seen:
+                return False
+
+        return True 
