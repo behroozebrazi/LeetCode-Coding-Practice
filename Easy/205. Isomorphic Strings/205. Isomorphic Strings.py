1fedee016ba7ea9e375dccbd7508ad08bba1a0c6
behroozebrazi <behber@berkeley.edu>
2026-08-12 15:47:15 -0700

    205. Isomorphic Strings

--- Easy/205. Isomorphic Strings/205. Isomorphic Strings.py
+++ Easy/205. Isomorphic Strings/205. Isomorphic Strings.py
@@ -1,2 +1,21 @@
 # https://leetcode.com/problems/isomorphic-strings
 
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        noRepeatNum = set()
+        pattern = dict()
+
+        for index, sLetter in enumerate(s):
+            tLetter = t[index]
+
+            if sLetter in pattern:
+                if tLetter != pattern[sLetter]:
+                    return False
+
+            else:
+                if tLetter in noRepeatNum:
+                    return False
+                noRepeatNum.add(tLetter)
+                pattern[sLetter] = tLetter
+
+        return True
