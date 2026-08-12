f305a3efdae20ea141582b95c8f4697a4fecb420
behroozebrazi <behber@berkeley.edu>
2026-08-12 16:43:46 -0700

    49. Group Anagrams

--- Medium/49. Group Anagrams/49. Group Anagrams.py
+++ Medium/49. Group Anagrams/49. Group Anagrams.py
@@ -1,2 +1,12 @@
 # https://leetcode.com/problems/group-anagrams
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = "".join(sorted(word))
+            groups[key].append(word)
+
+        return list(groups.values())
