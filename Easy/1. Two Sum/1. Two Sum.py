c2dfe56dc8719a38ef1ec267dbb5030614eed4d9
behroozebrazi <behber@berkeley.edu>
2026-08-12 16:53:55 -0700

    1. Two Sum

--- Easy/1. Two Sum/1. Two Sum.py
+++ Easy/1. Two Sum/1. Two Sum.py
@@ -1,2 +1,11 @@
 # https://leetcode.com/problems/two-sum
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diffNums = dict()
+
+        for i, num in enumerate(nums):
+            if num in diffNums:
+                return [diffNums[num], i]
+
+            diffNums[target - num] = i
