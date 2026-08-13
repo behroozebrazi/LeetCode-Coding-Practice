794d077709d4ba3acd828573b30fe2cabce80ceb
behroozebrazi <behber@berkeley.edu>
2026-08-12 19:26:53 -0700

    228. Summary Ranges

--- Easy/228. Summary Ranges/228. Summary Ranges.py
+++ Easy/228. Summary Ranges/228. Summary Ranges.py
@@ -1,2 +1,25 @@
 # https://leetcode.com/problems/summary-ranges
 
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+
+        result = []
+
+        if len(nums) < 1:
+            return result
+
+        first = last = nums[0]
+
+        for num in nums:
+
+            if num - last > 1:
+                res = str(first) + ("->" + str(last) if first != last else "")
+                result.append(res)
+                first = num
+
+            last = num
+
+        res = str(first) + ("->" + str(last) if first != last else "")
+        result.append(res)
+
+        return result
