bb42273f085805dc85aba6aa80c9abc1c5564d0e
behroozebrazi <behber@berkeley.edu>
2026-08-14 18:11:07 -0700

    224. Basic Calculator

--- Hard/224. Basic Calculator/224. Basic Calculator.py
+++ Hard/224. Basic Calculator/224. Basic Calculator.py
@@ -1,2 +1,45 @@
 # https://leetcode.com/problems/basic-calculator
 
+class Solution:
+    def calculate(self, s: str) -> int:
+
+        result = 0
+        num = 0
+        sign = 1
+        stack = []
+
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+
+            elif c == '+':
+                result += sign * num
+                num, sign = 0, 1
+
+            elif c == '-':
+                result += sign * num
+                num, sign = 0, -1
+
+            elif c == '(':
+                # Save the current result and sign
+                stack.append(result)
+                stack.append(sign)
+
+                # Start a new calculation inside parentheses
+                result, sign = 0, 1
+
+            elif c == ')':
+                # Finish the number inside parentheses
+                result += sign * num
+                num = 0
+
+                # Restore the previous sign and result
+                previous_sign = stack.pop()
+                previous_result = stack.pop()
+
+                result = previous_result + previous_sign * result
+
+        # Add the final number
+        result += sign * num
+
+        return result
