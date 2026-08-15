ef956046dfc066a76912ebd4fc8d78af7c8f48ff
behroozebrazi <behber@berkeley.edu>
2026-08-14 17:41:37 -0700

    150. Evaluate Reverse Polish Notation

--- Medium/150. Evaluate Reverse Polish Notation/150. Evaluate Reverse Polish Notation.py
+++ Medium/150. Evaluate Reverse Polish Notation/150. Evaluate Reverse Polish Notation.py
@@ -1,2 +1,25 @@
 # https://leetcode.com/problems/evaluate-reverse-polish-notation
 
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+
+        result = 0
+        stack = []
+
+        for token in tokens:
+
+            match token:
+                case '+':
+                    stack.append(stack.pop() + stack.pop())
+                case '-':
+                    num = stack.pop()
+                    stack.append(stack.pop() - num)
+                case '*':
+                    stack.append(stack.pop() * stack.pop())
+                case '/':
+                    num = stack.pop()
+                    stack.append(int(stack.pop() / num))
+                case _:
+                    stack.append(int(token))
+
+        return stack.pop()
