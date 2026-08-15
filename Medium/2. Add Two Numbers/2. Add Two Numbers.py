e0e1a797becf44f2ac71df83a09b0aae120a952a
behroozebrazi <behber@berkeley.edu>
2026-08-14 21:46:44 -0700

    2. Add Two Numbers

--- Medium/2. Add Two Numbers/2. Add Two Numbers.py
+++ Medium/2. Add Two Numbers/2. Add Two Numbers.py
@@ -1,2 +1,36 @@
 # https://leetcode.com/problems/add-two-numbers
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        result = ListNode()
+        curr = result
+        parity = 0
+
+        while l1 and l2:
+            parity = l1.val + l2.val + parity                                     
+            val = parity % 10
+            parity = parity // 10
+            curr.next = ListNode(val)
+            curr = curr.next
+            l1 = l1.next
+            l2 = l2.next
+
+        rest = l1 if l1 else l2
+        while rest:
+            parity = rest.val + parity                                     
+            val = parity % 10
+            parity = parity // 10
+            curr.next = ListNode(val)
+            curr = curr.next
+            rest = rest.next
+
+        if parity > 0:
+            curr.next = ListNode(parity)
+
+        return result.next
