c0bc67d4b514e7a503886a9ed5ed2521f8c002ca
behroozebrazi <behber@berkeley.edu>
2026-08-14 17:20:29 -0700

    71. Simplify Path

--- Medium/71. Simplify Path/71. Simplify Path.py
+++ Medium/71. Simplify Path/71. Simplify Path.py
@@ -1,2 +1,19 @@
 # https://leetcode.com/problems/simplify-path
 
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+
+        stack = []
+        directory = path.split('/')
+
+        for name in directory:
+            if not name or name == '.':
+                continue
+
+            if name == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(name)
+
+        return '/' + '/'.join(stack)
