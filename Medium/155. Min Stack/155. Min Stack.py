ee0a4409f18bbce6e35c5790b7a1fd6d86f328be
behroozebrazi <behber@berkeley.edu>
2026-08-14 17:25:34 -0700

    155. Min Stack

--- Medium/155. Min Stack/155. Min Stack.py
+++ Medium/155. Min Stack/155. Min Stack.py
@@ -1,2 +1,21 @@
 # https://leetcode.com/problems/min-stack
 
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value: int) -> None:
+        if self.stack and self.stack[-1][1] < value:
+            self.stack.append((value, self.stack[-1][1]))
+        else:
+            self.stack.append((value, value))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
