88b32cffbe73394a5ba7e9c449f958fd350cab25
behroozebrazi <behber@berkeley.edu>
2026-08-18 10:49:20 -0700

    112. Path Sum

--- Easy/112. Path Sum/112. Path Sum.py
+++ Easy/112. Path Sum/112. Path Sum.py
@@ -1,2 +1,30 @@
 # https://leetcode.com/problems/path-sum
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+        if not root:
+            return False
+
+        stack = [(root, 0)]
+
+        while stack:
+            node, nodeSum = stack.pop()
+
+            nodeSum += node.val
+
+            if nodeSum == targetSum and not node.left and not node.right:
+                return True
+
+            if node.left:
+                stack.append((node.left, nodeSum))
+            if node.right:
+                stack.append((node.right, nodeSum))
+
+        return False
