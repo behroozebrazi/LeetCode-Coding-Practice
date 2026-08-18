38cf2a733e52dae4c96b983a3e06934d6b626d8d
behroozebrazi <behber@berkeley.edu>
2026-08-18 11:24:50 -0700

    124. Binary Tree Maximum Path Sum

--- Hard/124. Binary Tree Maximum Path Sum/124. Binary Tree Maximum Path Sum.py
+++ Hard/124. Binary Tree Maximum Path Sum/124. Binary Tree Maximum Path Sum.py
@@ -1,2 +1,26 @@
 # https://leetcode.com/problems/binary-tree-maximum-path-sum
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        sumMax = root.val
+
+        def dfs(node: Optional[TreeNode]) -> int:
+            nonlocal sumMax
+
+            if not node: return 0
+
+            leftMax = max(dfs(node.left), 0)
+            rightMax = max(dfs(node.right), 0)
+            sumMax = max(leftMax + node.val + rightMax, sumMax)
+
+            return max(leftMax, rightMax) + node.val
+
+        dfs(root)
+
+        return sumMax
