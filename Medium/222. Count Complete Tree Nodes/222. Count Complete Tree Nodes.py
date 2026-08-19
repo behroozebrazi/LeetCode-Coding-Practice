9c15ba9c6b793c8652a46287cf96391636067268
behroozebrazi <behber@berkeley.edu>
2026-08-19 13:21:42 -0700

    222. Count Complete Tree Nodes

--- Medium/222. Count Complete Tree Nodes/222. Count Complete Tree Nodes.py
+++ Medium/222. Count Complete Tree Nodes/222. Count Complete Tree Nodes.py
@@ -1,2 +1,35 @@
 # https://leetcode.com/problems/count-complete-tree-nodes
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+
+        def left_height(node):
+            height = 0
+            while node:
+                height += 1
+                node = node.left
+            return height
+
+        def right_height(node):
+            height = 0
+            while node:
+                height += 1
+                node = node.right
+            return height
+
+        left = left_height(root)
+
+        if left == right_height(root):
+            return (2 ** left) - 1
+            # return (1 << left) - 1
+
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
