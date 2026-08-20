0094de39abcd5cba24ea77a8b8697f8bee239ddb
behroozebrazi <behber@berkeley.edu>
2026-08-20 16:50:57 -0700

    98. Validate Binary Search Tree

--- Medium/98. Validate Binary Search Tree/98. Validate Binary Search Tree.py
+++ Medium/98. Validate Binary Search Tree/98. Validate Binary Search Tree.py
@@ -1,2 +1,51 @@
 # https://leetcode.com/problems/validate-binary-search-tree
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def dfs(node, low, high):
+            if not node:
+                return True
+
+            if node.val <= low or node.val >= high:
+                return False
+
+            return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+
+        return dfs(root, float("-inf"), float("inf"))
+
+
+
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+#         stack = []
+#         curr = root
+#         prev = None
+
+#         while stack or curr:
+
+#             while curr:
+#                 stack.append(curr)
+#                 curr = curr.left
+
+#             curr = stack.pop()
+#             if prev != None and prev.val >= curr.val:
+#                 return False
+
+#             prev = curr
+#             curr = curr.right
+
+#         return True
