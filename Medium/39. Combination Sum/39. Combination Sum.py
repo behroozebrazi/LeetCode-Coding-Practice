5f84cc756ed69d72e3c0a3f19ef5df2fa8d43211
behroozebrazi <behber@berkeley.edu>
2026-08-22 18:10:58 -0700

    39. Combination Sum

--- Medium/39. Combination Sum/39. Combination Sum.py
+++ Medium/39. Combination Sum/39. Combination Sum.py
@@ -1,2 +1,50 @@
 # https://leetcode.com/problems/combination-sum
 
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        result = []
+
+        def backtrack(start, current, total):
+
+            if target < total:
+                return
+
+            if target == total:
+                result.append(current.copy())
+                return
+
+            for i in range(start, len(candidates)):
+                num = candidates[i]
+                current.append(num)
+                backtrack(i, current, total + num)
+                current.pop()
+
+        backtrack(0, [], 0)
+
+        return result
+
+
+
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+#         result = set()
+#         min_num = min(candidates)
+#         max_length = target // min_num
+
+#         def backtrack(current, sum_num):
+#             if max_length < len(current) or target < sum_num:
+#                 return
+#             if sum_num == target:
+#                 result.add(tuple(sorted(current)))
+#                 return
+
+#             for num in candidates:
+#                 current.append(num)
+#                 backtrack(current, sum_num + num)
+#                 current.pop()
+
+#         backtrack([], 0)
+
+#         return list(result)
