f60c4cee6ff0f24d1c51facbea5db7d219e59a12
behroozebrazi <behber@berkeley.edu>
2026-08-22 17:36:30 -0700

    46. Permutations

--- Medium/46. Permutations/46. Permutations.py
+++ Medium/46. Permutations/46. Permutations.py
@@ -1,2 +1,21 @@
 # https://leetcode.com/problems/permutations
 
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        result = []
+
+        def backtrack(current, digits):
+            if len(digits) == 0:
+                result.append(current.copy())
+                return
+
+            for i in range(len(digits)):
+                new_nums = digits[:i] + digits[i+1:]
+                current.append(digits[i])
+                backtrack(current, new_nums)
+                current.pop()
+
+        backtrack([], nums)
+
+        return result
