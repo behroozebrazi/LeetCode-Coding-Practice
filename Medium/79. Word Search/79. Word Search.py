d00317682b938b224e6300053fc0b82f154ff746
behroozebrazi <behber@berkeley.edu>
2026-08-24 19:28:11 -0700

    79. Word Search

--- Medium/79. Word Search/79. Word Search.py
+++ Medium/79. Word Search/79. Word Search.py
@@ -1,2 +1,38 @@
 # https://leetcode.com/problems/word-search
 
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+
+            if (r < 0 or r >= rows) or (c < 0 or c >= cols) or (board[r][c] != word[i]):
+                return False
+
+            # Mark as visited
+            temp = board[r][c]
+            board[r][c] = "#"
+
+            # Explore 4 directions
+            found = (
+                dfs(r + 1, c, i + 1) or
+                dfs(r - 1, c, i + 1) or
+                dfs(r, c + 1, i + 1) or
+                dfs(r, c - 1, i + 1)
+            )
+
+            # Backtrack
+            board[r][c] = temp
+
+            return found
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+
+        return False
