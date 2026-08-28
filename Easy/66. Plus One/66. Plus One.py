19e871aefbe48145a7074ed127d1b633323bc9ee
behroozebrazi <behber@berkeley.edu>
2026-08-28 13:43:27 -0700

    66. Plus One

--- Easy/66. Plus One/66. Plus One.py
+++ Easy/66. Plus One/66. Plus One.py
@@ -1,2 +1,17 @@
 # https://leetcode.com/problems/plus-one
 
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        carry = 1
+        digit = len(digits)
+
+        while carry == 1 and digit > 0:
+            digit -= 1
+            carry, mod = divmod(digits[digit] + carry, 10)
+            digits[digit] = mod
+
+        if carry != 0:
+            digits.insert(0, carry)
+
+        return digits
