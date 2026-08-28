0fbba39c68f8b6cf69509056cbff9022adc047d7
behroozebrazi <behber@berkeley.edu>
2026-08-28 12:11:40 -0700

    136. Single Number

--- Easy/136. Single Number/136. Single Number.py
+++ Easy/136. Single Number/136. Single Number.py
@@ -1,2 +1,11 @@
 # https://leetcode.com/problems/single-number
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        result = 0
+
+        for num in nums:
+            result = result ^ num
+
+        return result
