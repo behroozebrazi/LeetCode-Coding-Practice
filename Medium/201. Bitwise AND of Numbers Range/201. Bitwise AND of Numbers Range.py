b1a8c3e1b4f085e386c142b673581cfdd0a9b2c9
behroozebrazi <behber@berkeley.edu>
2026-08-28 13:16:21 -0700

    201. Bitwise AND of Numbers Range

--- Medium/201. Bitwise AND of Numbers Range/201. Bitwise AND of Numbers Range.py
+++ Medium/201. Bitwise AND of Numbers Range/201. Bitwise AND of Numbers Range.py
@@ -1,2 +1,25 @@
 # https://leetcode.com/problems/bitwise-and-of-numbers-range
 
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+
+        # find common prefix
+        while left < right:
+            right = right & (right - 1)
+
+        return right
+
+
+
+# class Solution:
+#     def rangeBitwiseAnd(self, left: int, right: int) -> int:
+
+#         shift = 0
+
+#         # find common prefix
+#         while left != right:
+#             left >>= 1
+#             right >>= 1
+#             shift += 1
+
+#         return left << shift
