812cce66768038e23f21f161a6327e26a684a604
behroozebrazi <behber@berkeley.edu>
2026-08-28 13:35:06 -0700

    9. Palindrome Number

--- Easy/9. Palindrome Number/9. Palindrome Number.py
+++ Easy/9. Palindrome Number/9. Palindrome Number.py
@@ -1,2 +1,26 @@
 # https://leetcode.com/problems/palindrome-number
 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+
+        reverse_half = 0
+
+        while x > reverse_half:
+            x, mod = divmod(x, 10)
+            reverse_half = reverse_half * 10 + mod
+
+        return x == reverse_half or x == reverse_half // 10
+
+
+
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+        
+#         sf = str(x)
+#         sr = sf[::-1]
+#         half =  floor(len(sf) / 2)
+
+#         return sf[ : half] == sr[ : half]
