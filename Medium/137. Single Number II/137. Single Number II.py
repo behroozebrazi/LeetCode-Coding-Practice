7a8375e74d4fbabeb5d8fc955caae227b11d32c6
behroozebrazi <behber@berkeley.edu>
2026-08-28 12:38:59 -0700

    137. Single Number II

--- Medium/137. Single Number II/137. Single Number II.py
+++ Medium/137. Single Number II/137. Single Number II.py
@@ -1,2 +1,35 @@
 # https://leetcode.com/problems/single-number-ii
 
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        result = 0
+        one = 1
+
+        for _ in range(32):
+            total_ones = 0
+
+            for num in nums:
+                if num & one != 0:
+                    total_ones += 1
+
+            if total_ones % 3 == 1:
+                result = result | one
+
+            one = one << 1
+
+        return result - 2 ** 32 if result >= 2 ** 31 else result
+
+
+
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> int:
+
+#         ones = 0
+#         twos = 0
+
+#         for num in nums:
+#             ones = (ones ^ num) & ~twos
+#             twos = (twos ^ num) & ~ones
+
+#         return ones
