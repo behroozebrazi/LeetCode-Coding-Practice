9cb8424b14af5cc157a62a495b307fdd906371ec
behroozebrazi <behber@berkeley.edu>
2026-08-31 18:42:23 -0700

    149. Max Points on a Line

--- Hard/149. Max Points on a Line/149. Max Points on a Line.py
+++ Hard/149. Max Points on a Line/149. Max Points on a Line.py
@@ -1,2 +1,33 @@
 # https://leetcode.com/problems/max-points-on-a-line
 
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        if n <= 2:
+            return n
+
+        result = 0
+        for i in range(n):
+            slopes = defaultdict(int)
+
+            for j in range(i + 1, n):
+                dx = points[j][0] - points[i][0]
+                dy = points[j][1] - points[i][1]
+
+                if dx == 0:
+                    dy = 1
+                else:
+                    g = gcd(dx, dy)
+                    dx //= g
+                    dy //= g
+                    if dx < 0:
+                        dx = -dx
+                        dy = -dy
+
+                slope = (dy, dx)
+                count = slopes[slope] + 1
+                slopes[slope] = count
+                if result < count:
+                    result = count
+
+        return result + 1
