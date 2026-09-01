32060c63cd45e4b586edfd02d9709c930e9e9548
behroozebrazi <behber@berkeley.edu>
2026-08-31 21:23:50 -0700

    322. Coin Change

--- Medium/322. Coin Change/322. Coin Change.py
+++ Medium/322. Coin Change/322. Coin Change.py
@@ -1,2 +1,15 @@
 # https://leetcode.com/problems/coin-change
 
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        amount += 1
+        dp = [amount] * amount
+        dp[0] = 0
+
+        for i in range(1, len(dp)):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return dp[-1] if dp[-1] != amount else -1
