8f41a058d9110494195ca4bbdad0d42569bf4081
behroozebrazi <behber@berkeley.edu>
2026-08-31 20:02:17 -0700

    70. Climbing Stairs

--- Easy/70. Climbing Stairs/70. Climbing Stairs.py
+++ Easy/70. Climbing Stairs/70. Climbing Stairs.py
@@ -1,2 +1,15 @@
 # https://leetcode.com/problems/climbing-stairs
 
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        if n <= 2:
+            return n
+
+        first = 1
+        second = 2
+
+        for _ in range(n - 2):
+            first, second = second, first + second
+
+        return second
