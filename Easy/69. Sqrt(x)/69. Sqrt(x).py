68461a147f0b6ebed4ea649ebd8050b814822cdd
behroozebrazi <behber@berkeley.edu>
2026-08-31 17:39:11 -0700

    69. Sqrt(x)

--- Easy/69. Sqrt(x)/69. Sqrt(x).py
+++ Easy/69. Sqrt(x)/69. Sqrt(x).py
@@ -1,2 +1,23 @@
 # https://leetcode.com/problems/sqrtx
 
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        if x < 2:
+            return x
+
+        left = 2
+        right = x // 2
+
+        while left <= right:
+            mid = (left + right) // 2
+            power = mid * mid
+
+            if power < x:
+                left = mid + 1
+            elif power > x:
+                right = mid - 1
+            else:
+                return mid
+
+        return right
