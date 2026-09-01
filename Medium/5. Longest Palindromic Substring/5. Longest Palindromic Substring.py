52555dd7d37422857a78407e95245ea10f53c214
behroozebrazi <behber@berkeley.edu>
2026-09-01 06:43:14 -0700

    5. Longest Palindromic Substring

--- Medium/5. Longest Palindromic Substring/5. Longest Palindromic Substring.py
+++ Medium/5. Longest Palindromic Substring/5. Longest Palindromic Substring.py
@@ -1,2 +1,28 @@
 # https://leetcode.com/problems/longest-palindromic-substring
 
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+        if len(s) < 2:
+            return s
+
+        longest = ""
+
+        def expand(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        for i in range(len(s)):
+            # Odd-length palindrome
+            palindrome1 = expand(i, i)
+            if len(palindrome1) > len(longest):
+                longest = palindrome1
+
+            # Even-length palindrome
+            palindrome2 = expand(i, i + 1)
+            if len(palindrome2) > len(longest):
+                longest = palindrome2
+
+        return longest
