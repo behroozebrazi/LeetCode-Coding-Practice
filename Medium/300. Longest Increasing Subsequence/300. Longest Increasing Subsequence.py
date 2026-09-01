736946710bbc64b82021c93818b1f2dff57138f5
behroozebrazi <behber@berkeley.edu>
2026-08-31 22:14:51 -0700

    300. Longest Increasing Subsequence

--- Medium/300. Longest Increasing Subsequence/300. Longest Increasing Subsequence.py
+++ Medium/300. Longest Increasing Subsequence/300. Longest Increasing Subsequence.py
@@ -1,2 +1,33 @@
 # https://leetcode.com/problems/longest-increasing-subsequence
 
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        longest = 0
+        dp = [1] * len(nums)
+
+        for i in range(len(nums)):
+            local_max = 1
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    local_max = max(local_max, dp[j] + 1)
+            dp[i] = local_max
+            longest = max(longest, local_max)
+
+        return longest
+
+
+
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+
+#         tail = []
+
+#         for num in nums:
+#             idx = bisect_left(tail, num)
+#             if idx == len(tail):
+#                 tail.append(num)
+#             else:
+#                 tail[idx] = num
+
+#         return len(tail)
