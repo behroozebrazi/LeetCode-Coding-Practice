a98fdbcc2de6b817ebcbb538e65fc9cb68786720
behroozebrazi <behber@berkeley.edu>
2026-08-31 20:51:01 -0700

    139. Word Break

--- Medium/139. Word Break/139. Word Break.py
+++ Medium/139. Word Break/139. Word Break.py
@@ -1,2 +1,37 @@
 # https://leetcode.com/problems/word-break
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        words = set(wordDict)
+        word_lengths = {len(word) for word in words}
+
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for j in range(1, len(dp)):
+            for length in word_lengths:
+                i = j - length
+
+                if i >= 0 and dp[i] and s[i:j] in words:
+                    dp[j] = True
+                    break
+
+        return dp[-1]
+
+
+
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+#         words = set(wordDict)
+#         dp = [False] * (len(s) + 1)
+#         dp[0] = True
+
+#         for j in range(1, len(dp)):
+#             for i in range(j):
+#                 if dp[i] and s[i:j] in words:
+#                     dp[j] = True
+#                     break
+
+#         return dp[-1]
