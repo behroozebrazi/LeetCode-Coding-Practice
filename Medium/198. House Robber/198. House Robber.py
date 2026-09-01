e4b8ede89ab492a3bc9e70bc851209482a4eb69a
behroozebrazi <behber@berkeley.edu>
2026-08-31 20:22:02 -0700

    198. House Robber

--- Medium/198. House Robber/198. House Robber.py
+++ Medium/198. House Robber/198. House Robber.py
@@ -1,2 +1,14 @@
 # https://leetcode.com/problems/house-robber
 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+        nums.append(0)
+
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i - 1], nums[i] + nums[i - 2])
+
+        return nums[-1]
